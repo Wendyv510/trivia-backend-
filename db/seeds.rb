46ea1435cb90afb6ab5037eb9f46d3cfc7860133
Wendy Harris <wendyv510@hotmail.com>
2020-06-03 18:01:41 -0400

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,12 @@ boardGames = Category.create(name: "Board Games")
 movies = Category.create(name: "Movies") 
 
 
-Question.create(description: "This game involves black-and-white discs and a green grid for a board.", answer: "Othello", category_id: 1)
-Question.create(description: "In this game, you can be a dispatcher, medic, scientist, operations expert or researcher.", answer: "Pandemic", category_id: 1) 
-Question.create(description: "This game was immortalized in "When Harry Met Sally …"", answer: "Pictionary", category_id: 1) 
+Question.create(description: "This game involves black-and-white discs and a green grid for a board.", answer: "Othello", category_id: boardGames.id)
+Question.create(description: "In this game, you can be a dispatcher, medic, scientist, operations expert or researcher.", answer: "Pandemic", category_id: boardGames.id) 
+Question.create(description: "According to the official rules, how much money do you get when you land on Free Parking in Monopoly?", answer: "None", category_id: boardGames.id) 
 
-Question.create(description: "What are the dying words of Charles Foster Kane in Citizen Kane?", answer: "Rosebud", category_id: 2) 
-Question.create(description: "What was the first feature-length animated movie ever released?", answer: "Snow White and the Seven Dwarfs", category_id: 2) 
-Question.create(description: "In The Matrix, does Neo take the blue pill or the red pill?", answer: "Red", category_id: 2) 
+Question.create(description: "What are the dying words of Charles Foster Kane in Citizen Kane?", answer: "Rosebud", category_id: movies.id) 
+Question.create(description: "What was the first feature-length animated movie ever released?", answer: "Snow White and the Seven Dwarfs", category_id: movies.id) 
+Question.create(description: "In The Matrix, does Neo take the blue pill or the red pill?", answer: "Red", category_id: movies.id) 
 
 
