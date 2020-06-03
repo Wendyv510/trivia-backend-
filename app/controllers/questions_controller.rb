9e6ab39959f91cedcbb9cbec8e65518ce7cdea82
Wendy Harris <wendyv510@hotmail.com>
2020-06-03 13:51:14 -0400

    question controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,38 @@
 class QuestionsController < ApplicationController
+
+    def index 
+        @questions = Question.all 
+    end 
+
+    def new 
+        @question = Question.new 
+    end 
+
+    def create 
+        @question = Question.create(category_params) 
+    end 
+
+    def show 
+        @Question = Question.find(params[:id]) 
+    end 
+
+    def edit 
+        @question = Question.find(params[:id]) 
+    end 
+
+    def update
+        @question = Question.find(params[:id])
+        @question.update(name: params[:name])
+    end 
+    
+    def destroy 
+        @question = Question.find(params[:id])
+        @question.destroy 
+    end 
+
+      private 
+
+    def question_params 
+        params.require(:question).permit(:description, :answer, :category_id) 
+    end 
 end
