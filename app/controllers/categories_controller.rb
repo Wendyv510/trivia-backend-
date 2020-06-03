61200488f31123fbb23350c34a14c10c92cc5274
Wendy Harris <wendyv510@hotmail.com>
2020-06-03 13:47:13 -0400

    category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,39 @@
 class CategoriesController < ApplicationController
+
+    def index 
+        @categories = Category.all 
+    end 
+
+    def new 
+        @category = Category.new 
+    end 
+
+    def create 
+        @category = Category.create(category_params) 
+    end 
+
+    def show 
+        @category = Category.find(params[:id]) 
+    end 
+
+    def edit 
+        @category = Category.find(params[:id]) 
+    end 
+
+    def update
+        @category = Category.find(params[:id])
+        @category.update(name: params[:name])
+    end 
+    
+    def destroy 
+        @category = Category.find(params[:id])
+        @category.destroy 
+    end 
+
+      private 
+
+    def category_params 
+        params.require(:category).permit(:name) 
+    end 
+
 end
