0c069f0651cdf5bd17b6da7bf52cfcb9d4379bfd
Wendy Harris <wendyv510@hotmail.com>
2020-06-04 14:49:15 -0400

    category controller

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,3 +1,30 @@
 class Api::V1::CategoriesController < ApplicationController
+
+
+    def index 
+        @categories = Category.all 
+        render json: @categories 
+    end 
+
+    def new 
+        @category = Category.new 
+    end 
+
+    def create 
+        @category = Category.create(category_params)
+          if category.save 
+            render json: category, status: :accepted 
+          else 
+            render json: {errors: category.errors.full_messages}, status: :unproccessible_entity 
     
+          end 
+    end 
+
+    private 
+
+    def category_params 
+        params.require(:category).permit(:name) 
+    end 
+
+
 end
