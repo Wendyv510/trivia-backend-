b7b85a85d57cc9455846b9eb6c851df9c41f90e2
Wendy Harris <wendyv510@hotmail.com>
2020-06-06 11:02:11 -0400

    questions controller

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,3 +1,30 @@
 class Api::V1::QuestionsController < ApplicationController
     
+
+    def index 
+        questions = Question.all 
+        render json: QuestionSerializer.new(categories) 
+    end 
+
+    def new 
+        question = Question.new 
+    end 
+
+    def create 
+        question = Question.create(question_params)
+          if question.save 
+            render json: question, status: :accepted 
+          else 
+            render json: {errors: question.errors.full_messages}, status: :unproccessible_entity 
+    
+          end 
+    end 
+
+    private 
+
+    def question_params 
+        params.require(:question).permit(:description, :answer, :category_id) 
+    end 
+
+
 end
