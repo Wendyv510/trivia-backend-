24e5b247b67ad73cb29387f6c08b85a48e4fa51b
Wendy Harris <wendyv510@hotmail.com>
2020-06-09 17:19:14 -0400

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,9 +15,13 @@ movies = Category.create(name: "Movies")
 Question.create(description: "This game involves black-and-white discs and a green grid for a board.", answer: "Othello", category_id: boardGames.id)
 Question.create(description: "In this game, you can be a dispatcher, medic, scientist, operations expert or researcher.", answer: "Pandemic", category_id: boardGames.id) 
 Question.create(description: "According to the official rules, how much money do you get when you land on Free Parking in Monopoly?", answer: "None", category_id: boardGames.id) 
+Question.create(description: "Which of the following tokens was not one of the ten original game pieces in Monopoly?", answer: "Dog", category_id: boardGames.id) 
+        Question.create(description: "Who is the unfortunate victim in the game Clue?", answer: "Mr.Boddy", category_id: boardGames.id) 
 
 Question.create(description: "What are the dying words of Charles Foster Kane in Citizen Kane?", answer: "Rosebud", category_id: movies.id) 
 Question.create(description: "What was the first feature-length animated movie ever released?", answer: "Snow White and the Seven Dwarfs", category_id: movies.id) 
 Question.create(description: "In The Matrix, does Neo take the blue pill or the red pill?", answer: "Red", category_id: movies.id) 
+Question.create(description: "What was the name of the boat in Jaws?", answer: "Orca", category_id: movies.id) 
+Question.create(description: "What animal was framed in the unfinished paint-by-number in Rizzo’s room in Grease?", answer: "A horse", category_id: movies.id) 
 
 
