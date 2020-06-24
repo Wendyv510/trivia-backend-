00d4455c5cc60d38b1d2bc93131ce8bc5e6763d7
Wendy Harris <wendyv510@hotmail.com>
2020-06-24 12:59:05 -0400

    users

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+
+    def index 
+        users = User.all 
+        render json: UserSerializer.new(user) 
+    end 
+
+    def new 
+        user = User.new 
+    end 
+
+    def create 
+        user = User.create(user_params)
+          if user.save 
+            render json: user, status: :accepted 
+          else 
+            render json: {errors: user.errors.full_messages}, status: :unproccessible_entity 
+    
+          end 
+    end 
+
+    def show 
+      user = User.find(params[:id]) 
+    end 
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:username, :score) 
+    end 
+
+
+end
